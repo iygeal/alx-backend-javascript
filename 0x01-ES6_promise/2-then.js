1b1493643705d0a68d6fcf32f5f76f6465a48cbe
Innocent Anozie <iygeal@gmail.com>
2024-08-07 00:57:51 +0100

    Using .then, .ctach and .finally methods

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,17 @@
+function handleResponseFromAPI(promise) {
+  return promise
+    .then((result) => {
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch((error) => {
+      return new Error();
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
+
+export default handleResponseFromAPI;
