2b272c1b140b462043785d23499086d036f0f07f
Innocent Anozie <iygeal@gmail.com>
2024-08-07 09:51:26 +0100

    Using Promise.allSettled() to handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,26 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  // Call the imported functions
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  // Return a Promise for both the user and photo promises
+  return Promise.allSettled([userPromise, photoPromise]).then((results) => {
+    // Return an array of results
+    return results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: result.status,
+          value: result.value,
+        };
+      } else {
+        return {
+          status: result.status,
+          reason: result.reason,
+        };
+      }
+    });
+  });
+}
