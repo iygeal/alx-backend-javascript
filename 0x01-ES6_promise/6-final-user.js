b0012e6fd4a881da957e5aa811d600844712ddf4
Innocent Anozie <iygeal@gmail.com>
2024-08-07 10:04:19 +0100

    Refactored

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,26 +1,21 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  // Call the imported functions
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
 
-  // Return a Promise for both the user and photo promises
-  return Promise.allSettled([userPromise, photoPromise]).then((results) => {
-    // Return an array of results
-    return results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return {
-          status: result.status,
-          value: result.value,
-        };
-      } else {
-        return {
-          status: result.status,
-          reason: `${result.reason}`,
-        };
-      }
-    });
-  });
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then(
+    (values) => {
+      const result = [];
+      values.forEach((element) => {
+        if (element.status === 'fulfilled') {
+          result.push({ status: element.status, value: element.value });
+        } else {
+          result.push({ status: element.status, value: `${element.reason}` });
+        }
+      });
+      return result;
+    }
+  );
 }
