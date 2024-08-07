0dcbd4e6a33f4e975e258eaa09e3de26d556a807
Innocent Anozie <iygeal@gmail.com>
2024-08-07 10:03:06 +0100

    Modified: Used template literal to convert rejection reason to a string

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup (firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   // Call the imported functions
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
@@ -13,12 +13,12 @@ export default function handleProfileSignup (firstName, lastName, fileName) {
       if (result.status === 'fulfilled') {
         return {
           status: result.status,
-          value: result.value
+          value: result.value,
         };
       } else {
         return {
           status: result.status,
-          reason: result.reason
+          reason: `${result.reason}`,
         };
       }
     });
