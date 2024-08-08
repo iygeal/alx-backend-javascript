e2b739ba1011041cc758e226eedc6f5bac6db7b9
Innocent Anozie <iygeal@gmail.com>
2024-08-08 21:52:40 +0100

    Refactored task 1 to directly return array of ClassRoom objects without assigning them to variables

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,5 @@
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-  const clsRoom1 = new ClassRoom(19);
-  const clsRoom2 = new ClassRoom(20);
-  const clsRoom3 = new ClassRoom(34);
-
-  return [clsRoom1, clsRoom2, clsRoom3];
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
