362b937edbf1808a5187402141b46d5189702ea7
Innocent Anozie <iygeal@gmail.com>
2024-08-09 00:49:23 +0100

    Task 6: First inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,27 @@
+// Import the Building class
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    // Call the parent class constructor with sqft
+    super(sqft);
+
+    // Assign floors to the instance
+    this._floors = floors;
+  }
+
+  // Getter for sqft (inherited from Building)
+  get sqft() {
+    return super.sqft;
+  }
+
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  // Override the evacuationWarningMessage method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
