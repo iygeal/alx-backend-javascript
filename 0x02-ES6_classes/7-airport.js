a61eabde75fda19bbfb5a5a980adfdac6de405ce
Innocent Anozie <iygeal@gmail.com>
2024-08-09 01:04:06 +0100

    Task 7: Default string rep (toString()) in action

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+export default class Airport {
+  constructor(name, code) {
+    // Assign the attributes to the instance
+    this._name = name;
+    this._code = code;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Getter for code
+  get code() {
+    return this._code;
+  }
+
+  // Override the default string representation
+  toString() {
+    return this._code;
+  }
+}
