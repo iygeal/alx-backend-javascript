4b8d44b1911e2384d4bea7f421570ed06ad2a6f0
Innocent Anozie <iygeal@gmail.com>
2024-08-13 08:27:33 +0100

    Comment out some snippets

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string') {
-    return '';
-  }
+  // if (!startString || typeof startString !== 'string') {
+  //   return '';
+  // }
   return Array.from(set)
     .filter((item) => item.startsWith(startString))
     .map((item) => item.slice(startString.length))
