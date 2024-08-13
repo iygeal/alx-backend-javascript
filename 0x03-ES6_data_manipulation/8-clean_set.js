b61ca096a0c171eb2ee20940031fdb9fef43076d
Innocent Anozie <iygeal@gmail.com>
2024-08-13 09:23:49 +0100

    Refactored task 8 to meet output

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string') {
+  if (typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
 
-  return Array.from(set)
-    .filter((item) => item.startsWith(startString) && item !== startString) // Additional safeguard
-    .map((item) => item.slice(startString.length))
+  return [...set]
+    .filter((str) => str && str.startsWith(startString))
+    .map((str) => str.slice(startString.length))
     .join('-');
 }
