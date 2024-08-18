a5626112b26ed136b217f5b317f527330524c1f6
Innocent Anozie <iygeal@gmail.com>
2024-08-18 21:57:59 +0100

    TASK 1: Teacher interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,19 @@
 interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [key: string]: any;
+  readonly firstName: string; // readonly: can only be set when the object is created
+  readonly lastName: string; // readonly: can only be set when the object is created
+  fullTimeEmployee: boolean; // mandatory field
+  yearsOfExperience?: number; // optional field
+  location: string; // mandatory field
+  [key: string]: any; // allows additional properties with any value
 }
+
+// Demonstration
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false, // Additional property not defined in the interface
+};
+
+console.log(teacher3);
