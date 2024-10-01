31a590f5030c5435a5619006f809addf02120848
Innocent Anozie <iygeal@gmail.com>
2024-10-01 08:12:44 +0100

    TASK 6: small HTTP server using Express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+// create an instance of an Express application
+const app = express();
+
+// Define a route for the root endpoint ('/')
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+// Make the server listen on port 1245
+app.listen(1245);
